e6cef16fced626e8b0bb81de67d0ad9c0b45eda0
Benjamin <bklaric15@gmail.com>
2023-06-04 17:48:37 +0000

    cnt 15 fertig

--- src/cnt15_rtl.vhd
+++ src/cnt15_rtl.vhd
@@ -17,6 +17,18 @@ end entity;
 
 architecture rtl of cnt15 is 
 
+signal d, q		: unsigned(3 downto 0); --besser mit unsigned zu arbeiten, weil man arithmetische Operationen machen kann(=, -, <, >, usw.)
+
+
 begin
-  
+
+q <= "0000" when rst_n = '0' else d when rising_edge(clk); --4 Flipflops initializieren
+
+d <= "1101" when ld_i = '1' else q when q = 0 else q - 1; --Eingaenge von Flipflops setzen in Abhaengigkeit von q, also wenn ld_1 = 1 ist,
+																			 --dann wird 13 eingeschrieben, wenn nicht, dann pruefen wir ob q = 0 ist, when nicht,
+																			 --dann zaehlen wir rueckwaerts. 
+
+done_o <= '1' when q = 0 else '0';								 --einfache Setzung von done_o in Abhaengigkeit von q.
+cnt_o <= std_ulogic_vector(q);									 --Setzen von cnt_o, die uns Zustand des Zaehlers zeigt.
+
 end architecture rtl;
